7273deb39a18d1aa7170d1c40df4bc90609a04fa
Gabriel Molina <gabrielmolina717@gmail.com>
2025-04-01 02:59:26 +0800

    refactored a function refactored the display function to only get the latest item in the array

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
 const myLibrary = [];
 
-// const bookOne = {
-//   id: crypto.randomUUID(),
-//   title: "power of habit",
-//   author: "charles",
-//   pages: 200,
-// };
-
-// const bookTwo = {
-//   id: crypto.randomUUID(),
-//   title: "test",
-//   author: "test",
-//   pages: 1223,
-// };
-
-// myLibrary.push(bookOne);
-// myLibrary.push(bookTwo);
-
 /**
  * Function Constructor
  * @constructor
@@ -39,8 +22,8 @@ function Book(id, title, author, pages, hasRead) {
 }
 
 /**
- * Creates new instance for the Book Constructor
- * Push the created instance into the myLibrary array
+ * Creates a new object for the Book Constructor
+ * Push the created object into the myLibrary array
  * @param {string} id
  * @param {string} title
  * @param {string} author
@@ -55,17 +38,18 @@ function addBookToLibrary(id, title, author, pages, hasRead) {
 }
 
 const dialog = document.querySelector("dialog");
+
 const addBookBtn = document.querySelector(".add-book-btn");
 addBookBtn.addEventListener("click", () => {
   dialog.showModal();
 });
 
-const form = document.querySelector("form");
-
 const cancelBtn = document.querySelector(".cancel-btn");
 cancelBtn.addEventListener("click", (e) => {
   dialog.close();
 });
+
+const form = document.querySelector("form");
 
 const titleInputField = document.querySelector("#title");
 const authorInputField = document.querySelector("#author");
@@ -83,41 +67,43 @@ form.addEventListener("submit", (e) => {
     hasReadInputField.checked
   );
 
-  display();
   cancelBtn.click();
+  display();
 });
 
 /**
- * Display the book objects stored in the myLibrary array
+ * Display the latest book object stored in the myLibrary array
  * @returns {void}
  */
 const bookContainer = document.querySelector(".book-container");
 function display() {
-  for (let i = 0; i < myLibrary.length; i++) {
-    let bookCard = document.createElement("div");
-    bookCard.classList.add("book-card");
+  if (myLibrary.length !== 0) {
+    for (let i = myLibrary.length - 1; i < myLibrary.length; i++) {
+      let bookCard = document.createElement("div");
+      bookCard.classList.add("book-card");
 
-    let titleParagraph = document.createElement("p");
-    let authorParagraph = document.createElement("p");
-    let pagesParagraph = document.createElement("p");
-    let hasReadParagraph = document.createElement("p");
+      let titleParagraph = document.createElement("p");
+      let authorParagraph = document.createElement("p");
+      let pagesParagraph = document.createElement("p");
+      let hasReadParagraph = document.createElement("p");
 
-    let titleTextNode = document.createTextNode(myLibrary[i].title);
-    let authorTextNode = document.createTextNode(myLibrary[i].author);
-    let pagesTextNode = document.createTextNode(myLibrary[i].pages);
-    let hasReadTextNode = document.createTextNode(myLibrary[i].hasRead);
+      let titleTextNode = document.createTextNode(myLibrary[i].title);
+      let authorTextNode = document.createTextNode(myLibrary[i].author);
+      let pagesTextNode = document.createTextNode(myLibrary[i].pages);
+      let hasReadTextNode = document.createTextNode(myLibrary[i].hasRead);
 
-    titleParagraph.appendChild(titleTextNode);
-    authorParagraph.appendChild(authorTextNode);
-    pagesParagraph.appendChild(pagesTextNode);
-    hasReadParagraph.appendChild(hasReadTextNode);
+      titleParagraph.appendChild(titleTextNode);
+      authorParagraph.appendChild(authorTextNode);
+      pagesParagraph.appendChild(pagesTextNode);
+      hasReadParagraph.appendChild(hasReadTextNode);
 
-    bookCard.appendChild(titleParagraph);
-    bookCard.appendChild(authorParagraph);
-    bookCard.appendChild(pagesParagraph);
-    bookCard.appendChild(hasReadParagraph);
+      bookCard.appendChild(titleParagraph);
+      bookCard.appendChild(authorParagraph);
+      bookCard.appendChild(pagesParagraph);
+      bookCard.appendChild(hasReadParagraph);
 
-    bookContainer.appendChild(bookCard);
+      bookContainer.appendChild(bookCard);
+    }
   }
 }
 
